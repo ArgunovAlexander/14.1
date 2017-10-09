efce79022480e59c5bb7f85d810cfcbeb96a23f6
ArgunovAlexander <30368617+ArgunovAlexander@users.noreply.github.com>
2017-10-09 11:57:32 +0300

    Create main14.java

---
+++ src/main14.java
@@ -0,0 +1,9 @@
+public class Main14 {
+    　
+    public static void main (String[] args) {
+        Box myBox=new Box();
+        myBox.setWidth(60);
+        myBox.setHeight(20);
+	      myBox.ser1(myBox);
+    }
+}
